3fb4fbb831b9b559794943e1b9798ae50b7b322c
twopplhk <twopplhk@Harikrushnas-MacBook-Air.local>
2023-06-01 13:15:45 +0530

    cast the discount type

--- src/service/Models/DiscountCoupon.php
+++ src/service/Models/DiscountCoupon.php
@@ -6,9 +6,14 @@ use Immera\EcomDiscount\Service\Enums\DiscountType;
 
 class DiscountCoupon extends Model
 {
+
+    protected $casts = [
+        'discount_type' => DiscountType::class
+    ];
+
     public function calc(int $total): float
     {
-        return match(DiscountType::from($this->discount_type)) {
+        return match($this->discount_type) {
             DiscountType::PERCENT => floatval($total*$this->discount_value/100),
             DiscountType::AMOUNT => min($this->discount_value, $total),
             default => 0,
